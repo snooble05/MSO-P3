48bef151c892eb9658727dd57ed988e38482c05b
snooble05 <j.kaptijn@students.uu.nl>
2024-11-06 22:16:02 +0100

    In the process of adding functionality to the input textbox, only thing that isn't working yet is correctly setting the commands inside of repeat commands

--- MSO-P3/CommandField.cs
+++ MSO-P3/CommandField.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using System.Diagnostics;
 
 namespace MSO_P3
 {
@@ -27,6 +29,7 @@ namespace MSO_P3
 			_commandInput.Location = new Point(this.Location.X, this.Location.Y);
 			_commandInput.AcceptsTab = true;
 			_commandInput.Multiline = true;
+			_commandInput.ShortcutsEnabled = true;
 
 			this.Controls.Add(_commandInput);
 
@@ -36,16 +39,80 @@ namespace MSO_P3
 			_runButton.FlatStyle = FlatStyle.Flat;
 			_runButton.FlatAppearance.BorderColor = Color.Black;
 			_runButton.Size = new Size(100, 40);
-			_runButton.Click += runField;
+			_runButton.Click += runInput;
 			this.Controls.Add(_runButton);
 
 			this.Resize += resize;
 			this.resize(null!, null!);
 		}
 
-		private void runField(object o, EventArgs ea)
+		private void runInput(object o, EventArgs ea)
 		{
-			//TBA
+			_commands.Clear();
+			string[] lines = _commandInput.Text.TrimEnd().Split('\n');
+			for (int i = 0; i < lines.Length; i++)
+			{
+				string[] commandString = lines[i].Split([" ", "\t"], StringSplitOptions.RemoveEmptyEntries);
+				string command = commandString[0];
+				string addOn = commandString[1];
+
+				switch (command)
+				{
+					case "Move":
+						Commands.Add(new MoveCommand(Convert.ToInt32(addOn)));
+						break;
+					case "Turn":
+						Commands.Add(new TurnCommand(addOn));
+						break;
+					case "Repeat":
+						Commands.Add(new RepeatCommand(ParseRepeatCommands(lines.Skip(i + 1).ToArray(), 1), Convert.ToInt32(addOn)));
+						break;
+					default:
+						break;
+						//throw new ArgumentException("Unknown command given");
+				}
+			}
+			foreach (ICommand test in Commands)
+			{
+				Debug.WriteLine(test.ToString());
+			}
+		}
+
+		private List<ICommand> ParseRepeatCommands(string[] remainingLines, int nestingLevel)
+		{
+			List<ICommand> commands = new List<ICommand>();
+
+			for(int i = 0; i < remainingLines.Length; i++)
+			{
+				string[] commandString = remainingLines[i].Split([" ", "\t"], StringSplitOptions.None);
+				if (commandString[nestingLevel] == String.Empty) break;
+				commandString = commandString.Skip(nestingLevel).ToArray();
+				string command = commandString[0];
+				string addOn = commandString[1];
+
+				switch (command)
+				{
+					case "Move":
+						commands.Add(new MoveCommand(Convert.ToInt32(addOn)));
+						break;
+					case "Turn":
+						commands.Add(new TurnCommand(addOn));
+						break;
+					case "Repeat":
+						commands.Add(new RepeatCommand(ParseRepeatCommands(remainingLines.Skip(i + 1).ToArray(), nestingLevel + 1), Convert.ToInt32(addOn)));
+						break;
+					default:
+						break;
+				}
+
+				if (i + 1 == remainingLines.Length) break;
+				//Debug.WriteLine(remainingLines[i + 1]);
+				if (!(remainingLines[i + 1].StartsWith(' ') || remainingLines[i + 1].StartsWith('\t')))
+				{
+					break;
+				}
+			}
+			return commands;
 		}
 
 		private void resize(object o, EventArgs ea)
--- MSO-P3/obj/Debug/net8.0-windows/MSO P3.AssemblyInfo.cs
+++ MSO-P3/obj/Debug/net8.0-windows/MSO P3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSO P3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e89e009a86ea11f06f2fe83f255a8f99ea0179")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab4440518a2702bcf25d13c9035c5dfa09d6727")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSO P3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSO P3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
